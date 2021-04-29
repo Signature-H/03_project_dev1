184a5290b8ac28c60781ff0e363e72e2bf43746e
signatureh <signature-h@hanmail.net>
2021-04-29 18:05:41 +0900

    공지사항 리스트 기능 추가  - article-mapping.xml / ArticleDAO / ArticleServiceImpl / ArticleService / ArticleController 코드 추가  - readArticleForm.jsp 공지사항 리스트 추가(테스트용)

--- src/main/java/com/dev1/springproject/article/ArticleService.java
+++ src/main/java/com/dev1/springproject/article/ArticleService.java
@@ -8,6 +8,7 @@ public interface ArticleService {
 	public ArticleVO readArticle(ArticleVO vo);
 	public int countArticle(ArticlePageVO vo);
 	public List<ArticleVO> articleList(ArticlePageVO vo);
+	public List<ArticleVO> noticeList(ArticlePageVO vo);
 	public void deleteArticle(ArticleVO vo);
 	public void regNotice(ArticleVO vo);
 	public void unregNotice(ArticleVO vo);
